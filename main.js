bd13894ccc95ec4f4e01e071004176916a1e008c
DuStein <steindu@gmail.com>
2022-07-29 20:49:02 -0300

    botao clear funcionando

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let gameArea = document.querySelector('#gameArea')
-
-
-for(i=1; i<=16; i++) {
+const gameArea = document.querySelector('#gameArea')
+const btnDimension = document.querySelector('#dimension')
+const btnClear = document.querySelector('#clear')
+let size = 6;
+for(i=1; i<=size; i++) {
   let square = document.createElement('div')
   square.setAttribute('class', 'coluna')
   square.setAttribute('id', `coluna${i}`)
   gameArea.appendChild(square)
   let coluna = document.querySelector(`#coluna${i}`)
-  for(e=1; e<=16; e++) {
+  for(e=1; e<=size; e++) {
     let square = document.createElement('div')
     square.setAttribute('class', 'linha')
     square.setAttribute('id', `linha${e}`)
@@ -16,9 +17,16 @@ for(i=1; i<=16; i++) {
   }
 }
 
+//para colorir no hover do mouse
 const quadrados = document.querySelectorAll('.linha');
 quadrados.forEach(quadrado => {
   quadrado.addEventListener('mouseover', () => {
     quadrado.setAttribute('style', 'background-color: red;')
   })
+})
+
+//botao para limpar - recarrega a pagina
+btnClear.addEventListener('click', () => {
+  btnClear.setAttribute('style', 'background-color: white;')
+  window.location.reload()
 })
